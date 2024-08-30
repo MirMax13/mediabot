ee1ad2b4b08d25307f3d53fc7c76c4b2cfea5a57
Maxym Matskiv <matskiv2005@gmail.com>
2024-08-30 22:23:21 +0300

    update environment variables

--- db.py
+++ db.py
@@ -5,7 +5,8 @@ import os
 
 load_dotenv()
 MONGODB_PASSWORD=os.environ.get('MONGODB_PASSWORD')
-uri = f'mongodb+srv://Mir_Max:{MONGODB_PASSWORD}@mediabot.ksszufh.mongodb.net/?retryWrites=true&w=majority&appName=MediaBot'
+NICKNAME=os.environ.get('NICKNAME')
+uri = f'mongodb+srv://{NICKNAME}:{MONGODB_PASSWORD}@mediabot.ksszufh.mongodb.net/?retryWrites=true&w=majority&appName=MediaBot'
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['mediadatabase']
 
