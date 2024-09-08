de7de7de7c7c58606dc72f9141c24b385d812863
Maxym Matskiv <matskiv2005@gmail.com>
2024-09-08 15:54:18 +0300

    Refactor command names for adding movies and games. Upgraded start command output

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ from commands.start import send_welcome
 from commands.search_movie import search_movie
 
 
-bot.message_handler(commands=['addmovie'])(add_movie)
-bot.message_handler(commands=['addgame'])(add_game)
+bot.message_handler(commands=['add_movie'])(add_movie)
+bot.message_handler(commands=['add_game'])(add_game)
 bot.message_handler(commands=['list'])(list_items)
-bot.message_handler(commands=['addmanymovies'])(add_many_movies)
-bot.message_handler(commands=['addmanygames'])(add_many_games)
+bot.message_handler(commands=['add_many_movies'])(add_many_movies)
+bot.message_handler(commands=['add_many_games'])(add_many_games)
 bot.message_handler(commands=['start'])(send_welcome)
-bot.message_handler(commands=['searchmovie'])(search_movie)
+bot.message_handler(commands=['search_movie'])(search_movie)
 
 
 bot.delete_webhook()
--- commands/start.py
+++ commands/start.py
@@ -1,5 +1,35 @@
 from config import bot
 
-@bot.message_handler(commands=['start', 'hello']) # TODO: fix using command while adding/editing
+# TODO: fix using command while adding/editing
 def send_welcome(message):
-        bot.reply_to(message, "Howdy, how are you doing?")
+        command_list = """
+        Привіт, ось список команд:
+
+        Допоміжні команди:
+        /start - допомога з команлами
+
+        Команди для додавання:
+        /add_movie - додати фільм
+        /add_game - додати гру
+        /add_many_movies - додати багато фільмів (лише адмін)
+        /add_many_games - додати багато ігор (лише адмін)
+
+        Команди для пошуку:
+        /search_movie - пошук фільму
+        /search_game - пошук гри
+
+        Команди для видалення:
+        /delete_movie - видалити фільм
+        /delete_game - видалити гру
+
+        Команди для редагування:
+        /edit_movie - редагувати фільм
+        /edit_game - редагувати гру
+        
+        Команди для перегляду:
+        /list - список фільмів та ігор
+
+        Команди для адміністрування:
+        /admin - адмін панель
+"""
+        bot.reply_to(message, command_list)
