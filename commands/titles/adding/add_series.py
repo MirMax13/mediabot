3d7a949a5cc0fd1f9df518b80efe5c868c666eff
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-04-27 19:01:57 +0300

    Renamed function to handle adding a new series

---
+++ commands/titles/adding/add_series.py
@@ -0,0 +1,10 @@
+from config import bot
+from functions.adding_updating import process_title
+from variables.globals import media_type
+
+def add_series(message):
+    chat_id = message.chat.id
+    media_type[chat_id] = 'serial'
+    msg = bot.send_message(chat_id, "Як називався серіал?")
+    bot.register_next_step_handler(msg, process_title)
+
