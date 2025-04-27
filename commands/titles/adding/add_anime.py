bd0a1e32ed0ce8b852d21abe4b5fc713909abdb1
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-04-27 21:22:10 +0300

    Fixed error with media type for series, anime and cartoons

--- commands/titles/adding/add_anime.py
+++ commands/titles/adding/add_anime.py
@@ -1,15 +1,17 @@
 from config import bot
 from functions.adding_updating import process_title
-from variables.globals import media_type
+from variables.globals import media_type, movie_type
 
 def add_anime_series(message):
     chat_id = message.chat.id
-    media_type[chat_id] = 'anime_series'
+    media_type[chat_id] = 'film'
     msg = bot.send_message(chat_id, "Як називалось аніме?")
+    movie_type[chat_id] = 'anime_series'
     bot.register_next_step_handler(msg, process_title)
 
 def add_anime_movie(message):
     chat_id = message.chat.id
-    media_type[chat_id] = 'anime_movie'
+    media_type[chat_id] = 'film'
     msg = bot.send_message(chat_id, "Як називалось аніме?")
+    movie_type[chat_id] = 'anime_movie'
     bot.register_next_step_handler(msg, process_title)
--- commands/titles/adding/add_series.py
+++ commands/titles/adding/add_series.py
@@ -1,10 +1,11 @@
 from config import bot
 from functions.adding_updating import process_title
-from variables.globals import media_type
+from variables.globals import media_type, movie_type
 
 def add_series(message):
     chat_id = message.chat.id
-    media_type[chat_id] = 'serial'
+    media_type[chat_id] = 'film'
     msg = bot.send_message(chat_id, "Як називався серіал?")
+    movie_type[chat_id] = 'series'
     bot.register_next_step_handler(msg, process_title)
 
