94ed74da4c49f2c44373e5e6b8c7ce4cc86be959
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-04-27 19:01:37 +0300

    Rename and separate functions for adding anime series and movies

--- commands/titles/adding/add_anime.py
+++ commands/titles/adding/add_anime.py
@@ -2,9 +2,14 @@ from config import bot
 from functions.adding_updating import process_title
 from variables.globals import media_type
 
-def add_anime(message):
+def add_anime_series(message):
     chat_id = message.chat.id
-    media_type[chat_id] = 'anime'
+    media_type[chat_id] = 'anime_series'
     msg = bot.send_message(chat_id, "Як називалось аніме?")
     bot.register_next_step_handler(msg, process_title)
 
+def add_anime_movie(message):
+    chat_id = message.chat.id
+    media_type[chat_id] = 'anime_movie'
+    msg = bot.send_message(chat_id, "Як називалось аніме?")
+    bot.register_next_step_handler(msg, process_title)
