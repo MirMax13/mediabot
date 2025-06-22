ef2a6f706135192ef7525b25b2ab6eed5c98092c
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-06-22 12:23:54 +0300

    Refactor webhook handling and remove threading for correct bot execution

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
-import threading
-from flask import Flask
+from flask import Flask, request
+import telebot
 from config import bot
 import bot as bot_handlers
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def alive():
     return 'Bot is alive!', 200
 
-def run_bot():
-    bot.delete_webhook()
-    bot.infinity_polling()
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data(as_text=True)
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return '', 200
+    return 'Invalid request', 400
 
 if __name__ == '__main__':
-    threading.Thread(target=run_bot).start()
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
