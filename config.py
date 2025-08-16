23d399567d3c0be23d83e074bb83295b69fa6b7a
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-08-16 17:40:59 +0300

    Add BOT_MODE configuration to environment and config files

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 load_dotenv()
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_MODE = os.environ.get('BOT_MODE', 'polling')  # default polling
 bot = TeleBot(BOT_TOKEN)
 
 params_dict = defaultdict(dict)
