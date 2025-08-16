12a817bc8fedf1584637e170253984f5c6227b0e
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-08-16 18:33:26 +0300

    Added new function to return waiting list. Updated code to return only not waited list too

--- commands/list_items.py
+++ commands/list_items.py
@@ -5,13 +5,24 @@ from variables.globals import query_dict
 ADMIN_ID = os.environ.get('ADMIN_ID')
 
 def list_admin_items(message):
-    query = {'user_id': int(ADMIN_ID)}
+    query = {
+        'user_id': int(ADMIN_ID),
+        'status': {'$exists': False}
+    }
     query_dict[message.chat.id] = query
     list_items(message)
 
 def list_user_items(message):
     chat_id = message.chat.id
-    query = {'user_id': chat_id}
+    query = {
+        'user_id': chat_id,
+        'status': {'$exists': False}
+    }
     query_dict[chat_id] = query
     list_items(message)
 
+def list_waiting_items(message):
+    chat_id = message.chat.id
+    query = {'user_id': chat_id, 'status': 'waiting'}
+    query_dict[chat_id] = query
+    list_items(message)
